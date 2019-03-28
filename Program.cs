d2addb6afabf0c4ef636e66a38f8e76c2d69f97b
Jamie <jmorgan3475@gmail.com>
2019-03-28 09:52:18 -0600

    init

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace random_number
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      System.Console.WriteLine("Welcome to the Number Guesser");
+
+      bool playing = true;
+      Random rnd = new Random();
+
+      //keep track of moves
+      int moves = 0;
+
+      //Generate random number
+      int compNum = rnd.Next(1, 101);
+      //start game
+      while (playing)
+      {
+        //Allow player to enter number
+        System.Console.WriteLine(@"Pick a number: 
+        ");
+        string input = Console.ReadLine();
+
+        //determine if good input and compare user num to compNum
+        moves++;
+        int playerMove;
+        if (Int32.TryParse(input, out playerMove) && playerMove > 0 && playerMove < 101)
+        {
+          if (playerMove == compNum)
+          {
+            System.Console.WriteLine("That's correct!");
+            System.Console.WriteLine($"It took you {moves} moves to best the computer!");
+            // ask if they want to play again
+            System.Console.WriteLine(@"
+            Would you like to play again? (Y)es or (N)o?");
+            string playAgain = Console.ReadLine();
+            if (playAgain == "n")
+            {
+              System.Console.WriteLine("Thanks for playing!");
+              return;
+            }
+            else if (playAgain == "y")
+            {
+              moves = 0;
+              compNum = rnd.Next(1, 101);
+            }
+          }
+          else if (playerMove < compNum)
+          {
+            System.Console.WriteLine("Higher!");
+          }
+          else
+          {
+            System.Console.WriteLine("Lower!");
+          }
+
+        }
+        else
+        {
+          System.Console.WriteLine(@"
+          **Please provide a valid number from 1-100**");
+        }
+
+      }
+    }
+
+  }
+}
